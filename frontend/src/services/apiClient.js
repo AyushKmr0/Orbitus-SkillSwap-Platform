1ec8a25a14119db972b50306c7add13d31592481
Ayush <yeashr007@gmail.com>
2026-06-05 09:04:03 +0530

    add baseURL

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { store } from '../store/index.js';
 import { logout, updateAccessToken } from '../features/authSlice.js';
 
+axios.defaults.baseURL = 'https://orbitus-skillswap-platform.onrender.com';
 axios.defaults.withCredentials = true;
 
 let refreshPromise = null;
